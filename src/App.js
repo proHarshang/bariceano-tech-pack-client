f0b8d0c053eca09b04f164e7599612a39bfe3013
arpita chavda <arpitachavda013@gmail.com>
2024-12-30 18:49:57 +0530

    genarated pdf with some hard code data

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Auth from './pages/Auth';
 import TechPack from './pages/TechPack';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import TechPacksTable from './pages/TechPacksTable';
+import TechPackPdfGenerator from './TechPackPdfGenerator';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Auth />} />
+        <Route path='/test' element={<TechPackPdfGenerator />} />
         <Route path="/tech-pack" element={<TechPack />} />
         <Route path='/tech-pack-data' element={<TechPacksTable />} />
       </Routes>
