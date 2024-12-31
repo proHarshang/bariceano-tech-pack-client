557ce83151aec9abe7b9559feb879d9c9d0b13d3
Thakar-Devam <devamthakar.dev@gmail.com>
2024-12-31 17:06:37 +0530

    Modify

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import './App.css';
 import Auth from './pages/Auth';
+import { AuthProvider } from './context/AuthContext';
+import MainHeader from './common/main-header';
 import TechPack from './pages/TechPack';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import TechPacksTable from './pages/TechPacksTable';
 
+// Import React Query components
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// Create a new QueryClient instance
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Auth />} />
-        <Route path="/tech-pack" element={<TechPack />} />
-        <Route path='/tech-pack-data' element={<TechPacksTable />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <Router>
+          <MainHeader />
+          <Routes>
+            <Route path="/" element={<Auth />} />
+            <Route path="/tech-pack" element={<TechPack />} />
+            <Route path='/tech-pack-data' element={<TechPacksTable />} />
+          </Routes>
+        </Router>
+      </QueryClientProvider>
+    </AuthProvider>
   );
 }
 
