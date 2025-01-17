5f2ba658bf65a87955243f6c84031360cdb20591
Thakar-Devam <devamthakar.dev@gmail.com>
2025-01-17 11:42:43 +0530

    Refactor Layout3 component; adjust layout dimensions, enhance spacing, and improve accessibility for image upload areas

--- src/components/Layout3.jsx
+++ src/components/Layout3.jsx
@@ -39,16 +39,16 @@ const Layout3 = () => {
                     </div>
                 ))}
             </div>
-            <div className='flex justify-center gap-24'>
-                <div className='w-[35%] h-[150px]'>
+            <div className='flex gap-14'>
+                <div className='w-[50%] h-[140px]'>
                     <h1>Thread colour</h1>
                     <div className='w-full h-full flex justify-between gap-5'>
-                        {[0, 1].map((key, index) => (
+                        {[0, 1,2].map((key, index) => (
                             <div key={index} className='w-full h-full border-2 border-dashed rounded-2xl bg-[#F3F3F3] flex items-center justify-center' onClick={() => handleClick(index)}>
                                 {specSheet?.threadColorImages.find((item) => item.position === index) ? (
                                     <img src={specSheet?.threadColorImages.find((item) => item.position === index).src} alt={`thread-img-${index}`} className='object-cover h-full w-full rounded-2xl' />
                                 ) : (
-                                    <label className='text-sm flex flex-col items-center justify-center w-full h-full cursor-pointer'>
+                                    <label className='text-sm text-center flex flex-col items-center justify-center w-full h-full cursor-pointer'>
                                         <span>Drop an Image here</span>
                                     </label>
                                 )}
@@ -57,15 +57,15 @@ const Layout3 = () => {
                         ))}
                     </div>
                 </div>
-              <div className='w-[35%] h-[150px]'>
+              <div className='w-[50%] h-[140px]'>
                     <h1>Fabric colour</h1>
                     <div className='w-full h-full flex justify-between gap-5'>
-                        {[0, 1].map((key, index) => (
+                        {[0, 1,3].map((key, index) => (
                             <div key={index} className='w-full h-full border-2 border-dashed rounded-2xl bg-[#F3F3F3] flex items-center justify-center' onClick={() => handleClick(index)}>
                                 {specSheet?.fabricColorImages.find((item) => item.position === index) ? (
                                     <img src={specSheet?.fabricColorImages.find((item) => item.position === index).src} alt={`fabric-img-${index}`} className='object-cover h-full w-full rounded-2xl' />
                                 ) : (
-                                    <label className='text-sm flex flex-col items-center justify-center w-full h-full cursor-pointer'>
+                                    <label className='text-sm text-center flex flex-col items-center justify-center w-full h-full cursor-pointer'>
                                         <span>Drop an Image here</span>
                                     </label>
                                 )}
