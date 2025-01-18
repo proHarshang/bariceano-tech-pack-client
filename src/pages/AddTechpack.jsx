39cd253bbbb8335c6e86701a07abaf1f2a445464
Thakar-Devam <devamthakar.dev@gmail.com>
2025-01-18 17:47:52 +0530

    Add AddTechpack component; create initial structure for the AddTechpack page

---
+++ src/pages/AddTechpack.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+// import { Label, TextInput } from "flowbite-react";
+
+
+const AddTechpack = () => {
+  return (
+    <div>indexOfFirstItem</div>
+  )
+}
+
+export default AddTechpack
