59e1f6b943e015af7eebaf731c5eb3a2541731b3
Thakar-Devam <devamthakar.dev@gmail.com>
2025-01-23 11:19:38 +0530

    Refactor image handling in ArtworkPlacementSheet and SiliconeLabel components; improve image selector filtering and update button text for clarity

--- src/components/SiliconeLabel.jsx
+++ src/components/SiliconeLabel.jsx
@@ -21,7 +21,7 @@ const SiliconLabel = ({ page }) => {
                 />
             </div>
             <div className='h-[430px]'>
-                {slide.data.images[0] ? (
+                {slide.data.images[0].src ? (
                     <div className="flex flex-col items-center w-full h-full">
                         <img
                             onClick={() => setOpenPopupId(`images-0`)}
@@ -31,8 +31,8 @@ const SiliconLabel = ({ page }) => {
                         />
                     </div>
                 ) : (
-                    <div className="border-2 border-dashed bg-[#F3F3F3] border-gray-300 w-full h-full flex justify-center items-center cursor-pointer">
-                        <button type='button' onClick={() => setOpenPopupId(`images-0`)}>Add</button>
+                    <div onClick={() => setOpenPopupId(`images-0`)} className="border-2 border-dashed bg-[#F3F3F3] border-gray-300 w-full h-full flex justify-center items-center cursor-pointer">
+                        <button type='button'>Click to upload an image</button>
                     </div>
                 )}
             </div>
