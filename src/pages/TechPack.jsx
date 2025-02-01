22fa4127409c38532a80bd446a0479c5f1a7c03a
Harshang Thakar <harshang.developer@gmail.com>
2025-02-01 13:09:16 +0530

    In form "add slide" bug solved

--- src/pages/TechPack.jsx
+++ src/pages/TechPack.jsx
@@ -33,7 +33,7 @@ const TechPack = () => {
       if (!selectedLabels || !currentCategory || !currentSubCategory) {
         navigate('/', { replace: true });
       } else if (isSettingDataFetched) {
-        console.log(selectedLabels, currentCategory, currentSubCategory)
+        console.log("Selections from header ", selectedLabels, currentCategory, currentSubCategory)
 
 
         updateField("gender", currentSubCategory);
@@ -132,13 +132,21 @@ const TechPack = () => {
 
   const handleAddPage = () => {
     if (selectedPage) {
-      const type = getType(selectedPage, currentCategory, currentSubCategory)
-      addSlideAtIndex(selectedIndex, {
-        "page": 10,
-        "name": selectedPage.name,
-        "type": type,
-        "data": selectedPage.data,
-      });
+      selectedPage.data.images.map(item => {
+        addSlideAtIndex(selectedIndex, {
+          "page": 10,
+          "name": selectedPage.name,
+          "type": getType(selectedPage, currentCategory, currentSubCategory),
+          "data": {
+            "images": [
+              {
+                "position": 0,
+                "src": item.src
+              }
+            ]
+          }
+        });
+      })
     }
 
     setShowPopup(false);
@@ -173,7 +181,7 @@ const TechPack = () => {
   ];
 
   // Map the dynamic array to add the `type` property (same as `name`)
-  const dynamicArray = trims.map((item) => {
+  const dynamicArray = trims.map(item => {
     return {
       "name": item.name,
       "type": item.name,
@@ -185,8 +193,6 @@ const TechPack = () => {
 
   // Combine static and dynamic arrays
   const combinedArray = [...staticArray, ...dynamicArray];
-
-  console.log("combinedArray :", combinedArray)
 
   return (
     <form className="w-[841px] mx-auto mt-10" >
@@ -226,12 +232,14 @@ const TechPack = () => {
             </div>
             <div className="flex justify-end mt-10 space-x-2">
               <button
+                type='button'
                 onClick={() => setShowPopup(false)}
                 className="px-4 py-2 border rounded-md"
               >
                 Cancel
               </button>
               <button
+                type='button'
                 onClick={handleAddPage}
                 className="px-4 py-2 bg-black text-white rounded-md text-sm"
                 disabled={!selectedPage}
