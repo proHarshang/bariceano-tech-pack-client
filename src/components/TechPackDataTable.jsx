9f70646e6b778d39bcc8c862d2bcab7383eb768a
Thakar-Devam <devamthakar.dev@gmail.com>
2025-02-07 18:17:17 +0530

    Update comment button styles and replace SVG icons with FontAwesome components in TechPackDataTable

--- src/components/TechPackDataTable.jsx
+++ src/components/TechPackDataTable.jsx
@@ -443,9 +443,9 @@ const TechPackDataTable = ({ data = [], fetchTechPacks }) => {
                             )}
                         </div>
                     </div>
-                    <div className="w-[32px] h-[32px] pt-[32px]">
+                    <div className="w-[42px] h-[42px] pt-[32px]">
                         <label
-                            className={`cursor-pointer px-4 h-8 text-nowrap border border-black flex items-center justify-center ${isCommentChecked ? "bg-black text-white" : "bg-white text-black"}`}
+                            className={`cursor-pointer px-3 h-8 text-nowrap border border-black bg-transparent flex items-center justify-center `}
                         >
                             <input
                                 type="checkbox"
@@ -453,19 +453,8 @@ const TechPackDataTable = ({ data = [], fetchTechPacks }) => {
                                 className="hidden" // Hide the actual checkbox
                             />
                             {isCommentChecked ? (
-                                <span>
-                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M11.6666 10C11.6666 10.9205 10.9204 11.6667 9.99992 11.6667C9.07942 11.6667 8.33325 10.9205 8.33325 10C8.33325 9.07954 9.07942 8.33337 9.99992 8.33337C10.9204 8.33337 11.6666 9.07954 11.6666 10Z" fill="white" />
-                                        <path d="M10 16.3167C5.35917 16.3167 1.50109 13.6111 0.366076 10C1.50109 6.38899 5.35917 3.68337 10 3.68337C14.6409 3.68337 18.4989 6.38899 19.6339 10C18.4989 13.6111 14.6409 16.3167 10 16.3167ZM10 13.313C12.0585 13.313 13.6833 11.8635 13.6833 10C13.6833 8.13659 12.0585 6.68708 10 6.68708C7.94146 6.68708 6.31667 8.13658 6.31667 10C6.31667 11.8635 7.94146 13.313 10 13.313Z" stroke="white" strokeWidth="0.7" />
-                                    </svg>
-                                </span>
-                            ) : (
-                                <span>
-                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M13.7288 4.50667L15.4932 6.27103M14.8632 2.95241L10.0906 7.72504C9.84433 7.97135 9.67641 8.28505 9.60808 8.62658L9.16675 10.8333L11.3735 10.392C11.715 10.3237 12.0287 10.1558 12.275 9.9095L17.0477 5.13686C17.6509 4.53364 17.6509 3.55563 17.0477 2.95242C16.4444 2.3492 15.4664 2.34919 14.8632 2.95241Z" stroke="black" strokeLinecap="round" strokeLinejoin="round" />
-                                        <path d="M15.8333 12.5V15C15.8333 15.9205 15.0871 16.6666 14.1666 16.6666H4.99992C4.07944 16.6666 3.33325 15.9205 3.33325 15V5.83329C3.33325 4.91282 4.07944 4.16663 4.99992 4.16663H7.49992" stroke="black" strokeLinecap="round" strokeLinejoin="round" />
-                                    </svg>
-                                </span>
+                                <FaCommentDots className="size-7" />) : (
+                                <FaRegCommentDots className="size-7" />
                             )}
                         </label>
                     </div>
