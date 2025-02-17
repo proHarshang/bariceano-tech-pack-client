6ecb75cdf7793b2ee1f67ffbea5e6834c43cfa6b
Thakar-Devam <devamthakar.dev@gmail.com>
2025-02-17 18:13:32 +0530

    moDIFY PDF CODE

--- src/NewPDF.js
+++ src/NewPDF.js
@@ -2,12 +2,13 @@ import { useState } from 'react';
 import { AiOutlineLoading } from "react-icons/ai";
 import generatePdf from './utility/generatePDF';
 
-const TechPackPDFGenrate = (data) => {
-    const [isDownloading, setIsDownloading] = useState(false);    
+const TechPackPDFGenrate = ({ data }) => {
+    const [isDownloading, setIsDownloading] = useState(false);
+    
     return (
         <button
             type='button'
-            onClick={() => generatePdf(data.data, setIsDownloading)}
+            onClick={() => generatePdf(data, setIsDownloading)}
             className={`text-center flex items-center justify-center mx-auto ${isDownloading ? 'animate-spin cursor-not-allowed' : ''}`}
             disabled={isDownloading}
         >
