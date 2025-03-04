7a728fc8a88f917a97d146f5050eadeba239687b
Thakar-Devam <devamthakar.dev@gmail.com>
2025-03-04 15:29:26 +0530

    add

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,15 +1,19 @@
-this.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open("app-cache").then((cache) => {
-      return cache.addAll(["/", "/index.html"]);
-    })
-  );
+import { precacheAndRoute } from 'workbox-precaching';
+
+precacheAndRoute(self.__WB_MANIFEST);
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('app-cache').then((cache) => {
+            return cache.addAll(['/', '/index.html']);
+        })
+    );
 });
 
-this.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
